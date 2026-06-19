e6ce11146f27a93616eb8244104b667d7036e840
Emanuel Oly <olysancheznicolas@gmail.com>
2026-06-19 13:50:27 -0400

    (refactor): extraer mapeo duplicado en ObtenerReservasEstado usando MapearReserva

--- backend/Services/Reservas/ObtenerReservasEstado.cs
+++ backend/Services/Reservas/ObtenerReservasEstado.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using backend.Dtos.Habitaciones;
-using backend.Dtos.Huespedes;
 using backend.Dtos.Reservas;
 using backend.Repositories.Reservas;
 
@@ -21,50 +18,10 @@ namespace backend.Services.Reservas
         public async Task<List<ReservaResponseDto>> GetReservasEstado(string estadoReserva)
         {
             var reservas = await reservasRepository.GetReservasEstado(estadoReserva);
-            var reservasDtos = new List<ReservaResponseDto>();
-            foreach (var reserva in reservas)
-            {
-                var huespedes = reserva.Huespedes;
-                var huespedesDto = new List<HuespedResponseDto>();
-                foreach (var huesped in huespedes)
-                {
-                    var huespedDto = new HuespedResponseDto
-                    {
-                        Id = huesped.Id,
-                        Nombre = huesped.Nombre,
-                        Apellido = huesped.Apellido,
-                        Documento = huesped.Documento
-                    };
-                    huespedesDto.Add(huespedDto);
-                }
-                var habitacionDto = new HabitacionResponseSimpleDto
-                {
-                    Id = reserva.Habitacion.Id,
-                    TipoHabitacion = reserva.Habitacion.TipoHabitacion,
-                    NumHabitacion = reserva.Habitacion.NumHabitacion,
-                    CapacidadPersonas = reserva.Habitacion.CapacidadPersonas,
-                    Precio = reserva.Habitacion.Precio,
-                    TipoCama = reserva.Habitacion.TipoCama,
-                    Piso = reserva.Habitacion.Piso,
-                    EstadoHabitacion = reserva.Habitacion.EstadoHabitacion
-                };
-                var reservaDto = new ReservaResponseDto
-                {
-                    Id = reserva.Id,
-                    Huespedes = huespedesDto,
-                    HabitacionId = reserva.HabitacionId,
-                    EstadoReserva = reserva.EstadoReserva,
-                    FechaCheckInEsperado = reserva.FechaCheckInEsperado,
-                    FechaCheckOutEsperado = reserva.FechaCheckOutEsperado,
-                    PrecioTotal = reserva.PrecioTotal,
-                    Mora = reserva.Mora,
-                    Habitacion = habitacionDto
-                    
-                };
-                reservasDtos.Add(reservaDto);
-            }
 
-            return reservasDtos;
+            return reservas
+                .Select(MapearReserva.MapearReservaExec)
+                .ToList();
         }
     }
 }
